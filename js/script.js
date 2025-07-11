245eb4b0a062bca55e123a4256af99c4f562eeeb
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-11 17:12:13 +0200

    [UPD] genero-numeri

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 //recupero gli elementi del dom che mi servono
+const numbersList = document.getElementById(`numbers-list`);
 
+//genero i 5 numeri casuali
+let numbersGenerated = [];
 
-
+for (let i = 0; i < 6; i++) {
+  let randomNumber = parseInt(Math.random() * 50) + 1;
+  numbersGenerated.push(randomNumber);
+  numbersList.innerHTML += `<li>${randomNumber}</li>`;
+}
+console.log(numbersGenerated);
 
 //mostro le risposte
-function mostraNumeri() {
-  document.getElementById("answers-form").classList.remove("d-none");
-}
 
-mostraNumeri()
+//document.getElementById("answers-form").classList.remove("d-none");
+
+
+
