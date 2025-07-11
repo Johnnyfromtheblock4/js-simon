5f02fee8ebfd6e8353cc4a4736fa25df4b579137
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-11 17:47:11 +0200

    [UPD] impostazione-countdown

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ for (let i = 0; i < 6; i++) {
   numbersList.innerHTML += `<li>${randomNumber}</li>`;
 }
 
-
 //recupero l'elemento nel dom per mostrare il countdown
 const countdown = document.getElementById(`countdown`);
+const answerForm = document.getElementById(`answer-form`);
 
 //definisco una variabile che mi conta i secondi
 let seconds = 10;
@@ -20,22 +20,24 @@ let seconds = 10;
 //definisco il mio intervallo
 const interval = setInterval(() => {
   //controllo se i secondi sono = a 0
-  if (seconds === 0) {
+  if (seconds < 0) {
     //cancello l'intervallo
     clearInterval(interval);
+
+    //faccio sparire i numeri
+    numbersList.classList.add = "d-none";
+
     //faccio apparire i numeri da inserire
-    
+    answerForm.innerText = document
+      .getElementById("answers-form")
+      .classList.remove("d-none");
   } else {
     countdown.innerText = seconds;
   }
   //decremento della variabile seconds
   seconds--;
-}, 1000)
+}, 1000);
 
 //mostro le risposte
 
-
 //document.getElementById("answers-form").classList.remove("d-none");
-
-
-
