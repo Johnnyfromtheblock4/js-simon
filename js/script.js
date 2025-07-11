e5119cc96d16afdc09e0df6847299dba2bf6e024
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-11 16:38:54 +0200

    [UPD] mostro-risposte

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//recupero gli elementi del dom che mi servono
+
+
+
+
+//mostro le risposte
+function mostraNumeri() {
+  document.getElementById("answers-form").classList.remove("d-none");
+}
+
+mostraNumeri()
